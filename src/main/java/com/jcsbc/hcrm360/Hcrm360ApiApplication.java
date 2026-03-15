8af0988ccfca588e7830b930853670e453493659
veritas153 <kjyoung153@hotmail.com>
2026-03-15 14:58:01 +0900

    Initial commit

---
+++ src/main/java/com/jcsbc/hcrm360/Hcrm360ApiApplication.java
@@ -0,0 +1,13 @@
+package com.jcsbc.hcrm360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hcrm360ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hcrm360ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jcsbc/hcrm360/Hcrm360ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcsbc.hcrm360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hcrm360ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
